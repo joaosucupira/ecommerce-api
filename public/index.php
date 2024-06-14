c92d119decd138c7afec803810acc7e0076c8605
joao <jasucupira@example.com>
2024-06-14 12:23:04 -0300

    Forgot to declare

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ require __DIR__.'/../vendor/autoload.php';
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
 
-
+$app = require_once __DIR__.'/../bootstrap/app.php';
 // 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
